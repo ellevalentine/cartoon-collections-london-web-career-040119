d074a340c91bb3a7d3406890e476d4c6c4c776b4
Elle Valentine <elle.valentine@hotmail.com>
2019-03-27 23:38:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  
 end
+
+
 
 def summon_captain_planet# code an argument here
   # Your code here
 end
 
+
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
