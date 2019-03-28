79ce35c6770317d8c9bb8e71b04282b5aa0bd140
Elle Valentine <elle.valentine@hotmail.com>
2019-03-28 00:47:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,9 +23,12 @@ end
 
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(list)
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  list.include?(cheese_types)
+  
 end
 
 
