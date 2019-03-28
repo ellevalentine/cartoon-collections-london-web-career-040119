5a546d671c225017b43b1aafc3aef2b245d560d4
Elle Valentine <elle.valentine@hotmail.com>
2019-03-28 01:02:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(list, cheese_types)
  
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  if list.include?(cheese_types)
+  if list.find?(cheese_types)
     return 
   els return "nil"
 end
